0258fa841bf10275f0798ddaac023647a08b1235
Semiglas <congressmand@gmail.com>
2024-02-26 16:22:53 +0300

    fixed pull request templates etc

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
-import SearchSlice from '../slices/SearchSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import SearchSlice from "../slices/SearchSlice";
 
-import moviesApi from './../api/MoviesApi'
+import moviesApi from "./../api/MoviesApi";
 
 const store = configureStore({
-    reducer: {
-        [moviesApi.reducerPath]: moviesApi.reducer,
-        [SearchSlice.name]: SearchSlice.reducer,
-    },
-    middleware: getDefaultMiddleware =>
-        getDefaultMiddleware().concat(moviesApi.middleware),
-})
+  reducer: {
+    [moviesApi.reducerPath]: moviesApi.reducer,
+    [SearchSlice.name]: SearchSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(moviesApi.middleware),
+});
 
-export { store }
+export { store };
